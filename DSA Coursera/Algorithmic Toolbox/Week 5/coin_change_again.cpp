5ce7bcf59c7bb27b32b0ab5facaf178664bb5739
Dhruv Dubey <63750812+djdd01@users.noreply.github.com>
2021-03-30 19:25:49 +0530

    Removed output to a file

--- DSA Coursera/Algorithmic Toolbox/Week 5/coin_change_again.cpp
+++ DSA Coursera/Algorithmic Toolbox/Week 5/coin_change_again.cpp
@@ -46,11 +46,11 @@ int main()
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
-#ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
-    freopen("error.txt", "w", stderr);
-    freopen("output.txt", "w", stdout);
-#endif
+// #ifndef ONLINE_JUDGE
+//     freopen("input.txt", "r", stdin);
+//     freopen("error.txt", "w", stderr);
+//     freopen("output.txt", "w", stdout);
+// #endif
 
     int t = 1;
     /*is Single Test case?*/ //cin >> t;
